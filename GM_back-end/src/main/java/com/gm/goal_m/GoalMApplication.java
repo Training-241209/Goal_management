6d9e768e374b20f0c9495958ed898f0b776bcb4b
nickbutani <naitikbu@gmail.com>
2025-01-06 18:49:11 -0500

    added frontend and backend

---
+++ GM_back-end/src/main/java/com/gm/goal_m/GoalMApplication.java
@@ -0,0 +1,13 @@
+package com.gm.goal_m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalMApplication.class, args);
+	}
+
+}
