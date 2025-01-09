38881816314c41656baec9c9a8f5012aa70c5b96
john9u <duranjohn6@gmail.com>
2025-01-08 19:19:10 -0500

    Implement router setup and lazy loading for authentication routes

--- GM_front-end/src/routes/_auth/auth/login.lazy.tsx
+++ GM_front-end/src/routes/_auth/auth/login.lazy.tsx
@@ -1,9 +1,12 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import NotificationsSignInPageError from '../../../components/UI/signupPage'
 
 export const Route = createLazyFileRoute('/_auth/auth/login')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/_auth/auth/login"!</div>
+  return <div>
+    < NotificationsSignInPageError />
+  </div>
 }
