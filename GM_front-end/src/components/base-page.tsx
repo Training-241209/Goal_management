4374ba0fac3c10b2d4a3302c8600a70c07e35c80
nickbutani <naitikbu@gmail.com>
2025-01-09 00:44:45 -0500

    update base

--- GM_front-end/src/components/base-page.tsx
+++ GM_front-end/src/components/base-page.tsx
@@ -1,40 +1,49 @@
+import { LoginForm } from "@/features/auth/components/login-form";
 import { Navbar } from "./navbar";
 import { TypeAnimation } from "react-type-animation";
 
 export function BasePage() {
   return (
     <>
-      <Navbar />
-        <div className="flex items-center justify-center h-screen">
-            <div className="text-center mt-16">
-                <h1 className="text-5xl font-bold mb-6">Welcome to Aimzy!</h1>
-                <TypeAnimation
-                preRenderFirstString={true}
-                sequence={[
-                    "We will help you set objectives.",
-                    1000,
-                    "We will help you build routines.",
-                    1000,
-                    "We will help you track progress.",
-                    1000,
-                    "We will help you stay motivated.",
-                    1000,
-                    () => {
-                    console.log("Sequence completed");
-                    },
-                ]}
-                speed={50}
-                wrapper="span"
-                cursor={true}
-                repeat={Infinity}
-                style={{
-                    fontSize: "20px",
-                    display: "inline-block",
-                    fontFamily: "sans-serif",
-                }}
-                />
-            </div>
+      {/* <Navbar /> */}
+      <div className="flex items-center justify-center h-screen px-10">
+        <div className="flex lg:flex-row w-full justify-evenly items-center ">
+          {/* Welcome Text */}
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold mb-6">Welcome to Aimzy!</h1>
+            <TypeAnimation
+              preRenderFirstString={true}
+              sequence={[
+                "We will help you set objectives.",
+                1000,
+                "We will help you build routines.",
+                1000,
+                "We will help you track progress.",
+                1000,
+                "We will help you stay motivated.",
+                1000,
+                () => {
+                  console.log("Sequence completed");
+                },
+              ]}
+              speed={50}
+              wrapper="span"
+              cursor={true}
+              repeat={Infinity}
+              style={{
+                fontSize: "20px",
+                display: "inline-block",
+                fontFamily: "sans-serif",
+              }}
+            />
+          </div>
+
+          {/* Login Form */}
+          <div className="rounded-lg shadow-lg w-full max-w-sm">
+            <LoginForm />
+          </div>
         </div>
+      </div>
     </>
   );
 }
