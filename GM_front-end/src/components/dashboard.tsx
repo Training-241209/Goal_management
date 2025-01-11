01d6a551e1b3574108497182c1ee9c7107e96a31
nickbutani <naitikbu@gmail.com>
2025-01-11 16:15:01 -0500

    update dashboard

--- GM_front-end/src/components/dashboard.tsx
+++ GM_front-end/src/components/dashboard.tsx
@@ -1,4 +1,3 @@
-
 import { NewsCard } from "./newsCard";
 import { StatBox } from "./stateboxs";
 import { TaskTable } from "./taskTable";
@@ -8,7 +7,7 @@ import { Button } from "./ui/button";
 
 export function Dashboard(){
     return <>
-        <div className="bg-gray-100 h-screen m-0 p-5">
+        <div className="bg-gray-100 min-h-screen m-0 p-5 overflow-hidden">
             <div className="bg-white p-5 rounded-lg shadow-md">
                 <h1 className="text-4xl font-bold text-purple-900 mb-4">Today Task</h1>
                 <p className="text-gray-600 mb-6">Check your daily tasks and schedules</p>
@@ -18,7 +17,7 @@ export function Dashboard(){
             </div>
             
             {/* stateboxes */}
-            <div className= "mx-auto mt-10">
+            <div className= "mx-auto mt-10 overflow-hidden">
                 <div className="flex gap-12 flex-wrap">
                     <StatBox />
                     <StatBox />
@@ -26,7 +25,7 @@ export function Dashboard(){
                 </div>
             </div>
 
-            <div className="mx-auto mt-10">
+            <div className="mx-auto mt-10 overflow-hidden">
                 <div className="flex gap-12 flex-wrap ">
                     <NewsCard />
                     <TaskTable />
