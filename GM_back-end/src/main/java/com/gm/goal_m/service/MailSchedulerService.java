48441c668ef5a6d53879823d8a4a96b7047a63b9
Manatsa Chiomadzi <94277576+mccollegestudent@users.noreply.github.com>
2025-01-16 11:25:33 -0500

    added a mail scheduler

---
+++ GM_back-end/src/main/java/com/gm/goal_m/service/MailSchedulerService.java
@@ -0,0 +1,14 @@
+package com.gm.goal_m.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailSchedulerService {
+
+    public void sendEmails() {
+        System.out.println("schedular is working");
+    }
+
+    
+    
+}
