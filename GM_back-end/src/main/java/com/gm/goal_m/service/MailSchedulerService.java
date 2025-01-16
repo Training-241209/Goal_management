d6aa87019bfcbbe764a530179e5d141165a9a1fb
Manatsa Chiomadzi <94277576+mccollegestudent@users.noreply.github.com>
2025-01-16 16:38:12 -0500

    adding notification to be 5 minutes before start time

--- GM_back-end/src/main/java/com/gm/goal_m/service/MailSchedulerService.java
+++ GM_back-end/src/main/java/com/gm/goal_m/service/MailSchedulerService.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.gm.goal_m.model.Goal;
 import com.gm.goal_m.model.Task;
 import com.gm.goal_m.model.TimeFrame;
 import com.gm.goal_m.model.User;
@@ -19,62 +20,65 @@ public class MailSchedulerService {
     TaskService taskService;
     TimeFrameService timeFrameService;
     MailSenderService mailSenderService;
+    GoalService goalService;
 
-    public MailSchedulerService(UserService userService, TaskService taskService, TimeFrameService timeFrameService, MailSenderService mailSenderService){
+    public MailSchedulerService(UserService userService, TaskService taskService,TimeFrameService timeFrameService, MailSenderService mailSenderService,  GoalService goalService){
+
         this.userService = userService;
         this.taskService = taskService;
         this.timeFrameService = timeFrameService;
         this.mailSenderService = mailSenderService;
+        this.goalService = goalService;
 
     }
 
 
     public void sendEmails() {
-        System.out.println("schedular is working");
+        System.out.println("schedular is working/n/n");
 
 
 
-        // List<User> users = userService.getAllUsers();
+        List<User> users = userService.getAllUsers();
 
-        // for (User user: users){
-        //     LocalTime now = LocalTime.now();
+        for (User user: users){
+            LocalTime now = LocalTime.now();
 
-        //     System.out.println(user.getEmail());
-            // List <Task> tasks = taskService.getAllTasks();
+            if(user.getGoals().isEmpty())  continue;
 
-            // for(Task task: tasks){
-            //     List <TimeFrame> timeframes = timeFrameService.getAllTimeFrames();
+            for(Goal goal : user.getGoals()){
 
-            //     for(TimeFrame timeFrame : timeframes){
+                if(goal.getTasks().isEmpty())  continue;
 
-            //         Duration duration = Duration.between(timeFrame.getStartTime(), now);
+                for(Task task : goal.getTasks()){     
 
-            //         if (!duration.isNegative() && duration.toMinutes() <= 10 && !timeFrame.getStatus()) {
+                    if(task.getTimeFrames().isEmpty()) continue;
 
-            //             StringBuilder sb = new StringBuilder();
-            //             sb.append("Hi " + user.getFirstName()).append(",").append("\n");
-            //             sb.append("Your Scheduled Time is about to begin").append("\n");
-            //             sb.append("Task name: " + task.getName()).append("\n");
-            //             sb.append("Start time: " + timeFrame.getStartTime()).append("\n");
-            //             sb.append("End time: " + timeFrame.getEndTime()).append("\n");    
-                        
-            //             // mailSenderService.sendNewMail(user.getEmail(), task.getName(), sb.toString());
+                    for(TimeFrame timeFrame : task.getTimeFrames()){
+    
+                        Duration duration = Duration.between(timeFrame.getStartTime(), now);
+    
+                        if (!duration.isNegative() && duration.toMinutes() <= 5 && !timeFrame.getStatus()) {
+    
+                            StringBuilder sb = new StringBuilder();
+                            sb.append("Hi " + user.getFirstName()).append(",").append("\n");
+                            sb.append("Goal: " + goal);
+                            sb.append("Task name: " + task.getName()).append("\n");
+                            sb.append("Start time: " + timeFrame.getStartTime()).append("\n");
+                            sb.append("End time: " + timeFrame.getEndTime()).append("\n");    
+                            
+                            mailSenderService.sendNewMail(user.getEmail(), task.getName(), sb.toString());
+    
+                            System.out.println("timeframe present");
+                        }
+                    }
+    
+                }
 
-            //             System.out.println(sb.toString());
-            //         }
 
-
-            //         timeFrame.getStartTime();
-
-            //     }
-
-            // }
-
-
-        
+            }   
    
 
-        //  }
+         }
 
     
    
